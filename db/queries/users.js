36bbc9fe17ff864e1ea303191115055ffe0f0eee
Vicky John <vicky.d.john@email.com>
2023-07-21 22:26:25 -0700

    add route to add new password

--- db/queries/users.js
+++ db/queries/users.js
@@ -1,10 +1,20 @@
 const db = require('../connection');
 
-const getUsers = () => {
-  return db.query('SELECT * FROM users;')
-    .then(data => {
-      return data.rows;
-    });
-};
 
-module.exports = { getUsers };
+// add new password to database
+function addPassword(newPassword) {
+  return db
+    .query(`
+  INSERT INTO passwords (user_id, password, organization_id, categories_id)
+  VALUES ($1, $2, $3, $4)
+  RETURNING *;
+  `, [newPassword.user_id, newPassword.password, newPassword.organization_id, newPassword.categories_id])
+    .then(result => {
+      return result.rows[0];
+    })
+    .catch(err => {
+      console.log(err.message);
+    });
+}
+
+module.exports = { addPassword };
