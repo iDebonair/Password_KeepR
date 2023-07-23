6d82f5c827b53baae5547de50b5b3852eb8a19ea
iDebonair <ofemiowolabi@gmail.com>
2023-07-23 03:13:53 +0000

    Added edit and delete password routes

---
+++ routes/editPassword.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// ... (other middleware and configurations)
+
+// GET route to render the edit_passwords.ejs form
+app.get('/passwords/:id/edit', (req, res) => {
+  // fetch the password details by ID from your database
+  const passwordId = req.params.id;
+  const password = getPasswordById(passwordId); // To be replaced with the right function from sign on
+
+  // Pass the password object to the template
+  res.render('editPassword', { password });
+});
+
+// POST route to handle form submission
+app.post('/passwords/:id/edit', async (req, res) => {
+  try {
+    const passwordId = req.params.id;
+    const { website, username, password } = req.body;
+    await updatePasswordInDatabase(passwordId, { website, username, password }); // Replace updatePasswordInDatabase with your actual function
+
+    res.redirect(`/passwords/${passwordId}`);
+  } catch (error) {
+    console.error('Error updating password:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+const passwords = {};
+
+app.post('/passwords/:id/delete', (req, res) => {
+  const passwordId = req.params.id;
+  const password = passwordDatabase[passwordId];
+  const userId = req.session.user_id;
+
+  // Check if the user is logged in
+  if (!userId) {
+    res.status(401).end("Please log in to view the page");
+    } else {
+        delete passwordDatabase[passwordId];
+        res.redirect("/passwords");
+      }
+});
+
+
+
+module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -10,6 +10,22 @@ const router  = express.Router();
 
 router.get('/', (req, res) => {
   res.render('users');
-}); 
+});
+
+// Password Management Routes
+router.post('/passwords/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+   const passwordID = req.params.id;
+  } catch (error) {
+    console.error('Error adding password:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+router.get('/passwords/:id/edit', (req, res) => {
+  res.render('edit_passwords.ejs');
+  console.log ("Here is a succesful update", id)
+});
 
 module.exports = router;
