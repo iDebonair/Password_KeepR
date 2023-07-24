79526b9d8fddfd58a4c6d1a078bb79d21c882a86
jwanali <ciwan_ali@hotmail.com>
2023-07-24 00:18:28 -0500

    update signin/:id and add header to users page

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,16 @@
 
 const express = require('express');
 const router  = express.Router();
+const bcrypt = require("bcryptjs");
 const cookieSession = require("cookie-session");
+router.use(
+  cookieSession({
+    name: "session",
+    keys: ["aj345", "dfh345"],
+    // Cookie Options
+    //maxAge: 24 * 60 * 60 * 1000 expire 24 hours
+  })
+);
 const users = require('../db/queries/users');
 router.get('/', (req, res) => {
 
@@ -15,11 +24,25 @@ router.get('/', (req, res) => {
 }); 
 
 router.get('/signin/:id',(req,res) => {
+ 
   console.log(req.params.id);
-  console.log(users)
-
-  res.render('users')
+  const user_id = req.params.id
+  req.session.user_id = user_id;
+  console.log(req.session.user_id);
+  const templateVars = {
+    user: user_id,
+  };
+  console.log(user_id);
+  res.render('users',templateVars)
+});
+router.get('/signin/',(req,res) => {
+  req.session = null;
+  const templateVars = {
+    user: req.session,
+  };
+  res.render('users',templateVars)
 })
+
 
 
 
