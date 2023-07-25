6fa86f3222cf594dec078ac0c27ed7fcd55a1d96
Vicky John <vicky.d.john@email.com>
2023-07-25 16:20:19 -0700

    add route to display page and generate required password

--- routes/generatePassword.js
+++ routes/generatePassword.js
@@ -1,25 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const generatePassword = require('../db/queries/users')
+const { generatePassword } = require('../public/scripts/app');
 
+// Route to display the password generation form
 router.get('/generatepassword', (req, res) => {
-  res.render('generatePasswordForm');
-});
-
-router.post('/generatepassword', (req, res) => {
-
-  const passwordOptions = {
-    length: parseInt(req.body.length),
-    includeLowercase: req.body.includeLowercase === 'on',
-    includeUppercase: req.body.includeUppercase === 'on',
-    includeNumbers: req.body.includeNumbers === 'on',
-    includeSymbols: req.body.includeSymbols === 'on',
-  };
-
-  // password generator function
-  const generatedPassword = generatePassword(passwordOptions);
-
-  res.render('password_generated', { password: generatedPassword });
+  res.render('generatePasswordForm', { password : null});
 });
 
 module.exports = router;
