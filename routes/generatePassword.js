9290c8c512280da7ccb058b7b7341196e6e8b0fe
Vicky John <vicky.d.john@email.com>
2023-07-25 16:50:38 -0700

    add POST

--- routes/generatePassword.js
+++ routes/generatePassword.js
@@ -7,4 +7,24 @@ router.get('/generatepassword', (req, res) => {
   res.render('generatePasswordForm', { password : null});
 });
 
+// POST - Generate the password
+router.post('/generatepassword', (req, res) => {
+  const passwordOptions = {
+    length: parseInt(req.body.length),
+    includeLowercase: req.body.includeLowercase === 'on',
+    includeUppercase: req.body.includeUppercase === 'on',
+    includeNumbers: req.body.includeNumbers === 'on',
+    includeSymbols: req.body.includeSymbols === 'on',
+  };
+
+  try {
+    const password = generatePassword(passwordOptions);
+    console.log('Generated Password:', password);
+    res.render('generatePasswordForm', { password });
+  } catch (error) {
+    res.status(400).render('generatePasswordForm', { error: error.message, password: '' });
+  }
+});
+
+
 module.exports = router;
