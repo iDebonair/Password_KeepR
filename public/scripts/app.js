daeafafacdad8906f91bb9a075328b98e86950f2
Vicky John <vicky.d.john@email.com>
2023-07-26 17:49:30 -0700

    add link to add new password

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,74 @@
 // Client facing scripts here
+// add new password to database
+
+const db = require('/home/labber/password_keepr/db/connection');
+
+// Function to add a new password to the database
+async function addPassword(newPasswordData, loggedInUserName) {
+  const { appName, password, categoryName } = newPasswordData;
+
+  
+  try {
+    // Get the user_id from the users table based on the logged-in user's name
+    const userQuery = `
+      SELECT id, organization_id FROM users WHERE name = $1;
+    `;
+    const userResult = await db.query(userQuery, [loggedInUserName]);
+    const user = userResult.rows[0];
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    // Insert appName into the apps table and obtain the app_id
+    const appInsertQuery = `
+      INSERT INTO apps (name, categories_id)
+      VALUES ($1, (SELECT id FROM categories WHERE name = $2))
+      RETURNING id;
+    `;
+    const appInsertValues = [appName, categoryName];
+    const appInsertResult = await db.query(appInsertQuery, appInsertValues);
+    const appId = appInsertResult.rows[0].id;
+
+    // Insert the new password into the passwords table
+    const passwordInsertQuery = `
+      INSERT INTO passwords (user_id, password, organization_id, categories_id)
+      VALUES ($1, $2, $3, (SELECT id FROM categories WHERE name = $4))
+      RETURNING *;
+    `;
+    const passwordInsertValues = [user.id, password, user.organization_id, categoryName];
+    const passwordInsertResult = await db.query(passwordInsertQuery, passwordInsertValues);
+
+    return passwordInsertResult.rows[0];
+  } catch (error) {
+    throw error;
+  }
+}
+
+// function to generate random password
+function generatePassword(options) {
+  const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
+  const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const numberChars = '0123456789';
+  const symbolChars = '!@#$%^&*()-=_+[]{}|;:,.<>?';
+
+  let allChars = '';
+
+  if (options.includeLowercase) allChars += lowercaseChars;
+  if (options.includeUppercase) allChars += uppercaseChars;
+  if (options.includeNumbers) allChars += numberChars;
+  if (options.includeSymbols) allChars += symbolChars;
+
+  if (!allChars) {
+    throw new Error('Must include at least one character type.');
+  }
+
+  let password = '';
+  for (let i = 0; i < options.length; i++) {
+    const randomIndex = Math.floor(Math.random() * allChars.length);
+    password += allChars[randomIndex];
+  }
+
+  return password;
+}
+
--- routes/addPassword.js
+++ routes/addPassword.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const passwordController = require('../db/queries/users');
+const { addPassword } = require('../public/scripts/app');
 
 
 // GET - show the form to add a new password
@@ -9,8 +9,24 @@ router.get('/passwords/new', (req, res) => {
 });
 
 // POST - submit new password
-router.post('/passwords/:id', passwordController.addPassword);
+router.post('/password', (req, res) => {
+  // Extract the necessary data from the request body
+  const newAppPassword = {
+    appName: req.body.appName,
+    password: req.body.password,
+    categoryName: req.body.categoryName,
+    loggedInUserId: req.session.user_id
+  };
 
-
+  // Call the passwordController to add the password to the database
+  addPassword(newAppPassword)
+    .then(password => {
+      res.redirect('/signin');
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).send('Error adding password');
+    });
+});
 
 module.exports = router;
